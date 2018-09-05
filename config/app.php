778549843b249a347ef9d2e8c09efb0f8fb10ac2
Ivan <769964834@qq.com>
2018-09-05 14:56:01 +0800

    publish els provider

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        CrCms\ElasticSearch\LaravelServiceProvider::class,
+
+
     ],
 
     /*
