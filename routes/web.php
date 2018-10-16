dc7fb667026e5df0dbb6015633adcc98af519385
Ivan <769964834@qq.com>
2018-10-16 13:37:24 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -31,17 +31,14 @@ Route::get('/node/add/schools', 'ArticleController@index');
 
 
 Route::namespace('Admin')->prefix('admin')->group(function () {
-
     Route::get('users', 'UserController@index');
-
     Route::get('articles', 'ArticleController@index');
     Route::get('articles/{id}', 'ArticleController@show');
     Route::get('articles/store', 'ArticleController@store');
     Route::any('articles/modify', 'ArticleController@modify');
 
-
     Route::get('schools', 'SchoolController@index');
-    Route::any('schools/store', 'SchoolController@store');
+//    Route::any('schools/store', 'SchoolController@store');
     Route::get('schools/{id}', 'SchoolController@show');
     Route::any('schools/modify', 'SchoolController@modify');
 
